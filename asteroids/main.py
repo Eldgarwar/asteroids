b691741d4b0df89b503570dd34708cc9af002c67
Eldgarwar <joubertclementst@gmail.com>
2025-09-19 10:00:49 -0400

    added player class and drew the player

--- asteroids/main.py
+++ asteroids/main.py
@@ -3,11 +3,14 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+
+    player = Player(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -21,6 +24,8 @@ def main():
                 return
 
         screen.fill((0, 0, 0))
+        player.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000  # Amount of seconds between each loop
 
