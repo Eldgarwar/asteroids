1176139b8fefd0236965c48f6adf9089ca02c6ad
Eldgarwar <joubertclementst@gmail.com>
2025-09-19 09:49:49 -0400

    added circleshape.py

--- asteroids/main.py
+++ asteroids/main.py
@@ -11,6 +11,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -20,6 +22,7 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000  # Amount of seconds between each loop
 
 if __name__ == "__main__":
     main()
