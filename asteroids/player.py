9fe7a7ad1b87df4bda30c5f8931e976d814e29b7
Eldgarwar <joubertclementst@gmail.com>
2025-09-19 10:14:09 -0400

    added full movement and screen overlap

--- asteroids/player.py
+++ asteroids/player.py
@@ -17,3 +17,41 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
+
+    def rotate(self, direction, dt):
+        if direction == "left":
+            self.rotation -= PLAYER_TURN_SPEED * dt
+        elif direction == "right":
+            self.rotation += PLAYER_TURN_SPEED * dt
+        self.rotation %= 360  # keep it in the range [0, 360)
+
+    def move(self, direction, dt):
+        if direction == "forward":
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            self.position += forward * PLAYER_MOVE_SPEED * dt
+        elif direction == "backward":
+            forward = pygame.Vector2(0, 1).rotate(self.rotation)
+            self.position -= forward * PLAYER_MOVE_SPEED * dt
+
+        # Wrap around screen edges
+        if self.position.x < 0:
+            self.position.x = SCREEN_WIDTH
+        elif self.position.x > SCREEN_WIDTH:
+            self.position.x = 0
+        if self.position.y < 0:
+            self.position.y = SCREEN_HEIGHT
+        elif self.position.y > SCREEN_HEIGHT:
+            self.position.y = 0     
+            
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate("left", dt)
+        if keys[pygame.K_d]:
+            self.rotate("right", dt)
+        if keys[pygame.K_w]:
+            self.move("forward", dt)
+        if keys[pygame.K_s]:
+            self.move("backward", dt)
